0547929f331c1f1e5d37c8f03cd9cf1b46251df8
Dione <dionemorales@outlook.com>
2023-04-21 09:44:43 +1000

    add new keymaps and fix syntax

--- lua/dione/remap.lua
+++ lua/dione/remap.lua
@@ -3,6 +3,9 @@ vim.keymap.set("x", "p", "\"_dP")
 vim.keymap.set("n", "d", "\"_d")
 vim.keymap.set("n", "d", "\"_d")
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
