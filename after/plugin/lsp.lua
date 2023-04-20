b53289202345dfeb32cdef1245398d22692088ca
Dione <dionemorales@outlook.com>
2023-04-21 07:46:17 +1000

    update keymaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -18,7 +18,7 @@ cmp.setup({
 
 lsp.on_attach(function(client, bufnr)
 	local opts = {buffer = bufnr, remap = false}
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set("n", "gd", function() vim.lsp.bu.definition() end, opts)
 	vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
 	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
